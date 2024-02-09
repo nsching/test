54a720d6c2972b7ca9bcc462c8168b4b29b85af8
nsching <143790749+nsching@users.noreply.github.com>
2024-02-08 15:32:32 -1000

    edited

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
+function divide(list) {
+  // Group strings into two arrays based on length (even or odd)
+  const grouped = _.groupBy(list, function (input) {
+    return input.length % 2 === 0 ? 'even' : 'odd';
+  });
 
+  // Return arrays of arrays
+  return [grouped.even, grouped.odd];
+}
+
+// Input array
+const myList = ['apple', 'banana', 'orange', 'grape', 'kiwi', 'melon'];
+
+// Call the function and print the result
+console.log(divide(myList));
